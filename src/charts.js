68705223f1fe5494aa9d3e0b1dea54b02112ff3f
Frank Elavsky <frankelavsky@gmail.com>
2022-04-14 12:46:11 -0400

    upgrade line to full size

--- src/charts.js
+++ src/charts.js
@@ -1,79 +1,91 @@
 props = {
     'line-chart': {
-        mainTitle: '',
-        subTitle: '',
+        mainTitle: 'Product A is trending up this year',
+        subTitle: '(An example line chart that can be navigated with a keyboard and screen reader!)',
         valueAccessor: 'value',
         ordinalAccessor: 'date',
         seriesAccessor: 'category',
-        uniqueID: 'sparkline',
-        width: 36,
-        height: 18,
-        padding: { top: 0, bottom: 0, left: 0, right: 0},
-        margin: { top: 0, bottom: 0, left: 0, right: 0},
-        dataLabel: {visible: false},
-        xAxis: {visible: false},
-        yAxis: {visible: false},
-        showDots: false,
+        uniqueID: 'line',
+        padding: { top: 20, bottom: 20, left: 40, right: 80},
+        margin: { top: 0, bottom: 40, left: 0, right: 0},
+        colors: ["#fefeff"],
+        yAxis: { label: "", visible: false, gridVisible: false },
+        xAxis: { label: "", visible: true },
+        showBaselineX: false,
+        dataLabel: { visible: true, placement: "auto" },
+        animationConfig: { disabled: true },
+        highestHeadingLevel: 'h1',
+        legend: {visible: false},
+        accessibility: {
+            elementDescriptionAccessor: "Note",
+            longDescription:
+              "This line chart is trending upwards.",
+            executiveSummary:
+              "We started the year with a value of 12, but now our product value has more than tripled, to 39 total.",
+            elementsAreInterface: false,
+            disableValidation: true,
+            hideStrokes: false
+        },
         data: [
         {
             value: 12,
             date: 'Jan',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 13,
             date: 'Feb',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 15,
             date: 'Mar',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 14,
             date: 'Apr',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 14,
             date: 'May',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 18,
             date: 'Jun',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 17,
             date: 'Jul',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 19,
             date: 'Aug',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 24,
             date: 'Sep',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 23,
             date: 'Oct',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 29,
             date: 'Nov',
-            category: 'Product'
+            category: 'Product A'
         },
         {
             value: 39,
             date: 'Dec',
-            category: 'Product'
+            category: 'Product A'
         }
         ]
   }
